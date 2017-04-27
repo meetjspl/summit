9ce81c9c0db9579ac076094c22dde628db9948e4
Michal Miszczyszyn <michal@mmiszy.pl>
2017-04-27 13:37:22 +0200

    Deploy to ftp through travis and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,23 +11,46 @@ var gulp = require('gulp'),
   reload = browserSync.reload,
   browserify = require('browserify');
 
-gulp.task('html', function() {
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+
+gulp.task('deploy', function () {
+  var remotePath = '/2017/';
+  var conn = ftp.create({
+    host: 'meetjs.pl',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['build/**'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+});
+
+gulp.task('html', function () {
   return gulp.src(config.html)
-  .pipe(gulp.dest(config.tmp))
+    .pipe(gulp.dest(config.tmp))
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp
-  .src(config.sass)
-  .pipe($.plumber({errorHandler: function(err) {console.log(err); this.emit('end')}}))
-  .pipe($.sass({
-    includePaths: ['src/scss', 'node_modules']
-  }))
-  .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
-  .pipe(gulp.dest(config.tmp_css));
+    .src(config.sass)
+    .pipe($.plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end')
+      }
+    }))
+    .pipe($.sass({
+      includePaths: ['src/scss', 'node_modules']
+    }))
+    .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+    .pipe(gulp.dest(config.tmp_css));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   var bundler = browserify({
     entries: config.js[0],
     extensions: ['.js'],
@@ -43,17 +66,17 @@ gulp.task('js', function() {
     .pipe(gulp.dest(config.tmp_js));
 });
 
-gulp.task('eslint', function() {
+gulp.task('eslint', function () {
   return gulp.src(config.js)
     .pipe($.eslint({
       envs: ['es6'],
-      ecmaFeatures: { modules: true }
+      ecmaFeatures: {modules: true}
     }))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError())
 });
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync({
     open: false,
     server: {
@@ -61,36 +84,36 @@ gulp.task('browserSync', function() {
       routes: {
         '/vendors': 'vendors',
         '/images': 'src/images',
-        '/fonts' : 'src/fonts'
+        '/fonts': 'src/fonts'
       }
     }
   })
 });
 
-gulp.task('default', ['eslint', 'sass', 'html', 'js', 'browserSync'], function() {
+gulp.task('default', ['eslint', 'sass', 'html', 'js', 'browserSync'], function () {
   gulp.watch([config.sass[1]], ['sass', reload]);
   gulp.watch([config.html], ['html', reload]);
   gulp.watch([config.js[1]], ['js', reload]);
 });
 
-gulp.task('images', function(){
+gulp.task('images', function () {
   return gulp.src(config.images)
-  .pipe($.imagemin())
-  .pipe(gulp.dest(config.build_images))
+    .pipe($.imagemin())
+    .pipe(gulp.dest(config.build_images))
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
   return gulp.src(config.fonts)
-  .pipe(gulp.dest(config.build_fonts))
+    .pipe(gulp.dest(config.build_fonts))
 });
 
-gulp.task('build', ['html', 'sass', 'js', 'fonts', 'images'], function() {
+gulp.task('build', ['html', 'sass', 'js', 'fonts', 'images'], function () {
   var assets = $.useref.assets();
   return gulp.src(config.tmp_html)
-  .pipe(assets)
-  .pipe(gulpIf('*.css', $.cssnano()))
-  .pipe(gulpIf('*.js', $.uglify()))
-  .pipe(assets.restore())
-  .pipe($.useref())
-  .pipe(gulp.dest(config.build))
+    .pipe(assets)
+    .pipe(gulpIf('*.css', $.cssnano()))
+    .pipe(gulpIf('*.js', $.uglify()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest(config.build))
 });
