bd9ed552509aec22a66fba3de3d237eddce8d110
Michał Pierzchała <thymikee@gmail.com>
2017-05-26 20:29:53 +0200

    Add necessary polyfills

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+import 'core-js/modules/es6.array.from.js';
+import 'classlist-polyfill';
+
 import {initHamburgerMenu} from './hamburger.js';
 import {initStickyNav} from './sticky-nav.js';
 import {initGallery} from './about-gallery.js';
