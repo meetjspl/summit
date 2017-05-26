a20296c99f8264af1853b951eaecbba94fc36934
Dariusz Niemczyk <palid@o2.pl>
2017-05-26 20:36:20 +0200

    Fix jsless page with terrible, terrible hacks. (Fixes #27)

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -8,6 +8,7 @@ export function initScroll() {
   const menu = document.querySelector('.button-menu__menu-icon');
   const close = document.querySelector('.button-menu__close-icon');
   const header = document.querySelector('.main-header');
+  const menuCheckbox = document.getElementById('menu-check');
 
   let scrollOffset = 0;
   if (window.innerWidth < 678) {
@@ -23,7 +24,7 @@ export function initScroll() {
     if (!nav.classList.contains('main-header__nav--nav-open')) {
       return;
     }
-
+    menuCheckbox.checked = false;
     nav.classList.toggle('main-header__nav--nav-open');
     menu.classList.toggle('button-menu__menu-icon--nav-open');
     close.classList.toggle('button-menu__close-icon--nav-open');
