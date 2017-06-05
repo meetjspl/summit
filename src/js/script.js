73f8d78d1093fd044fc3d2f15e8fdb6107756e5a
Michal Miszczyszyn <michal@mmiszy.pl>
2017-06-06 01:35:19 +0200

    Fix various a11y related things

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,8 @@ import 'babel-polyfill';
 
 import {initHamburgerMenu} from './hamburger.js';
 import {initStickyNav} from './sticky-nav.js';
-import {initGallery} from './about-gallery.js';
 import {initScroll} from './scroll.js';
 
 initHamburgerMenu();
 initStickyNav();
-initGallery();
 initScroll();
