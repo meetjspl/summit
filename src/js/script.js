067e10d295dbaec65f7445c4c2a63bd742bf7505
Michal Miszczyszyn <michal@mmiszy.pl>
2018-07-17 16:46:42 +0200

    Get rid of smooth-scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
 import { initHamburgerMenu } from './hamburger.js';
 import { initStickyNav } from './sticky-nav.js';
-import { initScroll } from './scroll.js';
 import { initAgenda } from './agenda.js';
 
 initHamburgerMenu();
 initStickyNav();
-initScroll();
 initAgenda();
 
 import '../scss/main.scss';
