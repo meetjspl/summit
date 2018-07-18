ba0a57a9b646bd04306429b6b879a10b436f9985
Michał Miszczyszyn <mmiszy@users.noreply.github.com>
2018-07-18 22:55:12 +0200

    Update organisers (#34)
    * Add Wojtek and fix scroll
    
    * Add organisers
    
    * Google Analytics

--- webpack.prod.js
+++ webpack.prod.js
@@ -61,7 +61,7 @@ module.exports = {
       },
       {
         // Load all images as base64 encoding if they are smaller than 8192 bytes
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
         use: [
           {
             loader: 'url-loader',
