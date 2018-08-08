54d7ec04b294b0fcfb31fa89dc6eb571035b936d
Naugtur <naugtur@gmail.com>
2018-08-08 22:52:09 +0200

    set up deployment to exlpicitly copy sponsorship offer to the root of 2018

--- webpack.prod.js
+++ webpack.prod.js
@@ -151,6 +151,7 @@ module.exports = {
       { from: 'redirect.html', to: '../index.html' },
       { from: 'CNAME', to: '../' },
       { from: 'src/robots.txt', to: '..' },
+      { from: 'src/Summit-2018-sponsorship-offer.pdf', to: '.' },
       { from: 'src/images/facebook', to: 'images/facebook/' },
       { from: 'legacy-pages/', to: '..' },
     ]),
