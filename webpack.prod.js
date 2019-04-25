59cd47987fb9992439461061e7c158167501845a
Jakub Synowiec <github@jakubsynowiec.info>
2019-04-25 11:05:19 +0200

    Update Webpack config so it copies the PDF file

--- webpack.prod.js
+++ webpack.prod.js
@@ -151,7 +151,7 @@ module.exports = {
       { from: 'redirect.html', to: '../index.html' },
       { from: 'CNAME', to: '../' },
       { from: 'src/robots.txt', to: '..' },
-      { from: 'src/Summit-2018-sponsorship-offer.pdf', to: '.' },
+      { from: 'src/meetjs_summit_2019_sponsor_packages.pdf', to: '.' },
       { from: 'src/ticket.html', to: '.' },
       { from: 'src/images/facebook', to: 'images/facebook/' },
       { from: 'legacy-pages/', to: '..' },
