92ed5e75683f3cc1279b07a558195f07ed4c49d4
naugtur <naugtur@gmail.com>
2022-09-12 00:37:33 +0200

    sponsors loop

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,14 @@ Array.from(document.querySelectorAll('.js-toggle')).map(toggle => {
     e.target.parentNode.classList.toggle('js-toggle-open');
   });
 });
+
+if (window.location.hash === '#loop') {
+  console.log('looping sponsors');
+  let num = 0;
+  document.querySelector(`.js-sp-${num}`).classList.add('fullscreen');
+  setInterval(() => {
+    num = (num + 1) % 5;
+    document.querySelector('.fullscreen').classList.remove('fullscreen');
+    document.querySelector(`.js-sp-${num}`).classList.add('fullscreen');
+  }, 10000);
+}
