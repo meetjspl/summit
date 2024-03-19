6cf32d585ec70914778eb57222d078e2dc33968c
naugtur <naugtur@gmail.com>
2024-03-19 23:46:36 +0100

    looks acceptable

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,9 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const gitRevisionPlugin = new GitRevisionPlugin();
-
-const version = {
-  VERSION: gitRevisionPlugin.version(),
-  COMMITHASH: gitRevisionPlugin.commithash(),
-  BRANCH: gitRevisionPlugin.branch(),
-};
+const prod = require('./webpack.prod');
 
 module.exports = {
+  ...prod,
   mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
-  entry: {
-    main: './src/js/main.js',
-    agenda: './src/js/main-agenda.js',
-  },
   devServer: {
     port: 8085,
     contentBase: path.join(__dirname, 'dist'),
@@ -26,85 +11,4 @@ module.exports = {
       rewrites: [{ from: /^\/workshop$/, to: '/workshop.html' }],
     },
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|pdf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              limit: 8192,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      chunks: ['main'],
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: 'regulamin.html',
-      template: './src/regulamin.html',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'agenda.html',
-      template: './src/agenda.html',
-      chunks: ['agenda'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'workshop.html',
-      template: './src/workshop.html',
-      // chunks: ['workshop'],
-    }),
-
-    new CopyWebpackPlugin([{ from: 'src/pdf', to: 'pdf' }]),
-
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(version),
-    }),
-  ],
 };
