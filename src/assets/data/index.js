9abd8ca1296a9ec7d20dead8946447fc0b86ccf9
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-07 09:32:51 +0100

    prepare 2026 data

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,17 +1,5 @@
-import agendaData from './agenda.json';
 import mainData from './main.json';
-import speakersData from './speakers.json';
-import sponsorsData from './sponsors.json';
-
-// convert speakers array to a mapped object for easier lookup
-const speakersObject = {};
-speakersData.speakers.forEach((speaker) => {
-  speakersObject[speaker.id] = speaker;
-});
 
 export default {
   ...mainData,
-  speakers: speakersObject,
-  agenda: agendaData,
-  sponsors: sponsorsData,
 };
