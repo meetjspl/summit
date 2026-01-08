c966edbfccb916e37f8dc377f4be52f26042b29b
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-08 15:40:27 +0100

    Updates

--- 2026/cfp/2016/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-  header("X-Flag: qb lbh yvxr Qbhtynf Pebpxsbeq?");
-  header("X-Rant: we have way too many files and directories on this server");
-  include('index.html');
-?>
--- 2026/cfp/2016/jslint/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo 'contact us at contact@meetjs.pl with e-mail titled X-flag ticket for meet.js Summit';
-?>
