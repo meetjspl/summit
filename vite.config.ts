7b43c94bc31c76b2b0a9107963fa8881c1279523
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-16 00:07:50 +0100

    feat: ts path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import * as path from 'node:path';
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -14,4 +16,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
