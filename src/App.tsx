e9ae01cb0f52e1ab98a7c596c691222313a5eb89
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-15 14:38:09 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+export const App = () => {
+
+  return (
+    <h1 className='text-red-400'>Hello world!</h1>
+  );
+};
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']]
+      }
+    })
+  ]
+});
