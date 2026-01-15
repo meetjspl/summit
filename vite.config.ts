e5b51a480fd1bcdcf58732cda56256c38025f75d
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-15 14:54:37 +0100

    feat: prettier config & reformat

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']]
-      }
-    })
-  ]
+	plugins: [
+		tailwindcss(),
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']],
+			},
+		}),
+	],
 });
