54a0100b1339c73963ffa6035dc989d07fc0aedd
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-15 16:50:04 +0100

    feat: svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		svgr(),
 		react({
 			babel: {
 				plugins: [['babel-plugin-react-compiler']],
