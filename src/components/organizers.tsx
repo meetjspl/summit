6128a8d554582f5e03f2a8a8996376d50238ae5b
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-16 17:05:16 +0100

    feat: organizers

---
+++ src/components/organizers.tsx
@@ -0,0 +1,10 @@
+export const Organizers = () => {
+	return (
+		<p className="py-9 text-white md:py-12">
+			<span className="text-meetjs-blue">const</span> summit = {'{'}{' '}
+			<span className="text-accent">organizers</span>: ["
+			<span className="text-meetjs-blue">meet.js</span>", "brave", "
+			<span className="text-aidevs-green">ai_devs 4</span>"] {'}'};
+		</p>
+	);
+};
