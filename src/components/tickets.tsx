230f0e813276f73e0cc9c19430016dfb3fd18f63
Kamil Dzieniszewski <kamil.d@playsmart.io>
2026-01-16 18:03:30 +0100

    feat: tickets

--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -3,26 +3,34 @@ import { Wrapper } from '@/components/wrapper.tsx';
 
 export const Tickets = () => {
 	return (
-		<section id="tickets" className="relative bg-black text-white">
-			<div className="absolute top-1/2 h-32 w-full -rotate-2 bg-meetjs-green" />
+		<section id="tickets" className="relative bg-black py-16 text-white">
+			{/* Diagonal stripe background */}
+			<div className="absolute top-1/2 h-32 w-full -translate-y-1/2 -rotate-2 bg-meetjs-green md:h-40" />
+			
 			<Wrapper>
-				<h2 className="py-8 text-center text-5xl font-semibold">
+				<h2 className="relative z-10 mb-16 text-center text-4xl font-bold md:text-5xl">
 					Get Your Summit Ticket
 				</h2>
-				<div className="relative z-20 flex justify-around">
+
+				<div className="relative z-20 flex flex-col justify-center gap-8 md:flex-row">
 					<TicketVariant
 						title="Standard"
-						pack={['Welcome Pack', 'Access to the gastro area']}
+						subtitle="With a Standard ticket, you receive:"
+						pack={[
+							'Welcome Pack',
+							'Access to the gastro area'
+						]}
 						price={256}
 						link="#"
 					/>
 					<TicketVariant
-						title="VIP"
+						title="Premium"
+						subtitle="With a Premium ticket, you receive:"
 						pack={[
 							'Welcome Pack+',
 							'Access to the gastro area',
 							'Seating in VIP rows (2-4)',
-							'Launch at the bistro',
+							'Lunch at the bistro',
 						]}
 						price={512}
 						highlight={true}
