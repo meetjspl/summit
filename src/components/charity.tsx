1117bdb8fa55c94fe03841c883e4bfad2b55de6b
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-16 17:06:18 +0100

    feat: charity

---
+++ src/components/charity.tsx
@@ -0,0 +1,11 @@
+import { Wrapper } from '@/components/wrapper.tsx';
+
+export const Charity = () => {
+	return (
+		<section className="text-white">
+			<Wrapper>
+				<h2>Charity Event</h2>
+			</Wrapper>
+		</section>
+	);
+};
