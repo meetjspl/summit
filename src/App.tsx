654eca02babeb645a191ff7171a8f8f4b94d8898
Kamil Dzieniszewski <kamil.d@playsmart.io>
2026-01-16 18:32:28 +0100

    feat: speakers

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CFP } from '@/components/cfp.tsx';
 import { Charity } from '@/components/charity.tsx';
 import { InfoCards } from '@/components/info-cards.tsx';
 import { Partners } from '@/components/partners.tsx';
+import { Speakers } from '@/components/speakers.tsx';
 import { Venue } from '@/components/venue.tsx';
 
 import { Hero } from './components/hero.tsx';
@@ -52,6 +53,7 @@ export const App = () => {
 			<InfoCards />
 			<CFP />
 			<Partners partners={partners} />
+			<Speakers />
 			<Venue />
 			<Tickets />
 			<Charity />
