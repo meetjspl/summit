6adbc93da5f9682acaf00d14f63314fedeba67ac
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-18 17:53:03 +0100

    feat: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import * as path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
+import tanstackRouter from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
@@ -10,6 +11,10 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		svgr(),
+		tanstackRouter({
+			target: 'react',
+			autoCodeSplitting: true,
+		}),
 		react({
 			babel: {
 				plugins: [['babel-plugin-react-compiler']],
