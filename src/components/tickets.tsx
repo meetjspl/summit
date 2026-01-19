48c6de5e7b97d3673994500fbc4d0146e8d3f4fc
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-19 11:41:07 +0100

    feat: link easy cart tickets variant

--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -4,27 +4,22 @@ import { Wrapper } from '@/components/wrapper.tsx';
 export const Tickets = () => {
 	return (
 		<section id="tickets" className="relative bg-black py-16 text-white">
-			{/* Diagonal stripe background */}
 			<div className="absolute top-1/2 h-32 w-full -translate-y-1/2 -rotate-2 bg-meetjs-green md:h-40" />
-			
+
 			<Wrapper>
 				<h2 className="relative z-10 mb-16 text-center text-4xl font-bold md:text-5xl">
 					Get Your Summit Ticket
 				</h2>
-
 				<div className="relative z-20 flex flex-col justify-center gap-8 md:flex-row">
 					<TicketVariant
-						title="Standard"
+						title="Regular"
 						subtitle="With a Standard ticket, you receive:"
-						pack={[
-							'Welcome Pack',
-							'Access to the gastro area'
-						]}
+						pack={['Welcome Pack', 'Access to the gastro area']}
 						price={256}
-						link="#"
+						link="https://cart.easy.tools/checkout/brave/meet-js-summit?plan=price_1Sq8OSLfdutmLA8HuTjzDvMo"
 					/>
 					<TicketVariant
-						title="Premium"
+						title="VIP"
 						subtitle="With a Premium ticket, you receive:"
 						pack={[
 							'Welcome Pack+',
@@ -34,7 +29,7 @@ export const Tickets = () => {
 						]}
 						price={512}
 						highlight={true}
-						link="#"
+						link="https://cart.easy.tools/checkout/brave/meet-js-summit?plan=price_1Sq8OSLfdutmLA8HYBFn1jev"
 					/>
 				</div>
 			</Wrapper>
