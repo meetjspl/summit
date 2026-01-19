983a89b0316f8895d297e3d4112ae9136fc2b94e
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-19 21:07:16 +0100

    fix: discord link to perm

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -137,7 +137,7 @@ export const Footer = () => {
 									</svg>
 								</a>
 								<a
-									href="https://discord.gg/DSbpDhFg"
+									href="https://discord.gg/UycCSpRh6j"
 									target="_blank"
 									rel="noopener noreferrer"
 									className="text-white-2 transition-colors hover:text-meetjs-blue"
