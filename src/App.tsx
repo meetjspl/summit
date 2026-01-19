5c281b796ea7e56a29e86719264b4db38ae63b86
Kamil Dzieniszewski <kamil.d@playsmart.io>
2026-01-19 18:23:07 +0100

    feat: Add Call for Papers (CFP) section, including a new component, navigation link, and integration into the main application layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { CFP } from '@/components/cfp.tsx';
 import { Charity } from '@/components/charity.tsx';
 import { InfoCards } from '@/components/info-cards.tsx';
 import { Partners } from '@/components/partners.tsx';
@@ -59,6 +60,7 @@ export const App = () => {
 		<>
 			<Hero />
 			<InfoCards />
+			<CFP />
 			<Partners partners={partners} />
 			<Venue />
 			<Tickets />
