261a57fdfc15591b06e2474fd6aad17320e4c418
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2026-01-20 10:47:23 +0100

    feat: Add Crossweb to the list of sponsors. (#135)

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,11 @@ const partners = [
 		logoUrl: SocketDevLogo,
 		websiteUrl: 'https://socket.dev',
 	},
+	{
+		name: 'Crossweb',
+		logoUrl: 'https://crossweb.pl/upload/materialy/logo_podstawowe_mono_biale_JPG.jpg',
+		websiteUrl: 'https://crossweb.pl',
+	},
 ];
 
 export const App = () => {
