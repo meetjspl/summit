6d29475f330b9b7c0ad78d80da896dc8cffceadb
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-20 08:47:59 +0100

    feat: thank you details (#128)
    * feat: add thank-you page params from easy
    
    * feat: add order details on thank you page
    
    * fix: remove promo code
    
    * fix: validation
    
    * fix: bg image

--- src/components/thank-you.tsx
+++ src/components/thank-you.tsx
@@ -1,10 +1,13 @@
 import AnniversaryLogo from '@/assets/Anniversary-logo.svg?react';
 import { Organizers } from '@/components/organizers.tsx';
 import { Wrapper } from '@/components/wrapper.tsx';
+import { Route } from '@/routes/thank-you.tsx';
 
 import heroBg from '@/assets/SGH_ASCI.png';
 
 export const ThankYou = () => {
+	const { ec_order_id } = Route.useSearch();
+
 	return (
 		<header
 			className="hero relative flex min-h-screen flex-col overflow-hidden bg-cover bg-center md:bg-contain"
@@ -30,6 +33,9 @@ export const ThankYou = () => {
 				</Wrapper>
 			</div>
 			<Wrapper>
+				<p className="text-white">
+					Your order ID: <span className="font-bold">{ec_order_id}</span>*
+				</p>
 				<p className="py-4 text-center font-semibold text-white">
 					We're preparing something special for you, and we can't wait to meet
 					you on March 4th in Warsaw. Your ticket, in the form of a QR code,
@@ -47,6 +53,10 @@ export const ThankYou = () => {
 				>
 					Back to the main page
 				</a>
+				<p className="py-4 text-sm text-white">
+					* if you have any problem with this order use this order ID with
+					communication with us.
+				</p>
 			</Wrapper>
 		</header>
 	);
