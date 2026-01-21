5aa71d3f0c13c5efdbee5c242e27d98fd8234d2c
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-21 19:00:29 +0100

    fix: import type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { CFP } from '@/components/cfp.tsx';
 import { Charity } from '@/components/charity.tsx';
 import { InfoCards } from '@/components/info-cards.tsx';
-import { type Partner, Partners } from '@/components/partners.tsx';
+import { Partners } from '@/components/partners.tsx';
 import { Venue } from '@/components/venue.tsx';
 
 import { Hero } from './components/hero.tsx';
@@ -11,6 +11,8 @@ import SGHLogo from '/src/assets/partners/SGH.png';
 import AI_DevsLogo from '/src/assets/partners/ai_devs.png';
 import JetBrainsLogo from '/src/assets/partners/jetbrains-mono-white.svg';
 import SocketDevLogo from '/src/assets/partners/socket.png';
+
+import type { Partner } from '@/types/partner.ts';
 
 const partners: Partner[] = [
 	{
