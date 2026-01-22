9d5be4011fd4fcb88c46e72666b2631d90278291
Stanisław Synowiec <stasyn1410@gmail.com>
2026-01-22 10:07:17 +0100

    fix: add planby logo to agenda

--- src/components/agenda.tsx
+++ src/components/agenda.tsx
@@ -1,14 +1,23 @@
-import { Schedule } from "@/components/schedule/schedule";
-
+import PlanByLogo from '@/assets/partners/planby-pro-logo.svg';
+import { Schedule } from '@/components/schedule/schedule';
+import { Wrapper } from '@/components/wrapper.tsx';
 
 export const Agenda = () => {
 	return (
-		<section id="agenda" className="py-20 px-0 md:px-6">
-	   <div className="max-w-6xl mx-0 md:mx-auto">
-		 <div className="max-w-full md:max-w-2xl mx-0 md:mx-auto">
-		<Schedule />
-		 </div>
-	   </div>
-	 </section>
+		<section id="agenda" className="px-0 py-20 md:px-6">
+			<div className="mx-0 max-w-6xl md:mx-auto">
+				<div className="mx-0 max-w-full md:mx-auto md:max-w-2xl">
+					<Schedule />
+				</div>
+			</div>
+			<Wrapper>
+				<div className="flex items-center justify-center gap-4 p-4 text-white">
+					<p className="text-xl">schedule powered by</p>
+					<a href="https://planby.app/?utm_source=website&utm_medium=logo&utm_campaign=meetjs_summit_2026">
+						<img src={PlanByLogo} alt="planby" className="w-64" />
+					</a>
+				</div>
+			</Wrapper>
+		</section>
 	);
 };
