6efa3582c453683d584fea61ce74aa10e1be68ba
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2026-01-26 14:25:44 +0100

    fix: adjust anniversary text alignment and monitor positioning (#166)

--- src/components/anniversary.tsx
+++ src/components/anniversary.tsx
@@ -2,7 +2,7 @@ import { Wrapper } from '@/components/wrapper.tsx';
 
 export const Anniversary = () => {
 	return (
-		<div className="relative z-20 rotate-2 bg-meetjs-green py-2 text-4xl font-semibold text-black md:mt-4 md:text-7xl">
+		<div className="relative z-20 rotate-2 bg-meetjs-green py-2 text-center text-4xl font-semibold text-black md:mt-4 md:text-left md:text-8xl">
 			<Wrapper>
 				<p>
 					15<sup>th</sup> Anniversary
