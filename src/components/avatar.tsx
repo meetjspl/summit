d528f9bdbf1f8e2978eb09e95c559b2a2f1c377e
Stanisław Synowiec <stasyn1410@gmail.com>
2026-02-02 21:56:19 +0100

    feat: avatar generator

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,7 +1,41 @@
+import {toPng} from 'html-to-image';
+import {useRef} from 'react';
+
+import Frame from '@/assets/test.svg?react';
+import {DndImage} from '@/components/dnd-image.tsx';
+import {Wrapper} from '@/components/wrapper.tsx';
+
 export const Avatar = () => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	const exportPng = async () => {
+		if (!ref.current) return;
+		const dataUrl = await toPng(ref.current, {
+			pixelRatio: 2,
+			backgroundColor: 'transparent',
+			skipFonts: true,
+		});
+		const a = document.createElement('a');
+		a.href = dataUrl;
+		a.download = 'frame.png';
+		a.click();
+	};
+
 	return (
 		<section>
-			<h1>Avatar Component</h1>
+			<Wrapper>
+				<h1>Avatar Component</h1>
+				<div className="h-30" />
+				<div className="mx-auto w-full md:w-1/3">
+					<div className="relative" ref={ref}>
+						<DndImage />
+						<Frame className="pointer-events-none absolute top-0 left-0 h-full w-full" />
+					</div>
+					<button className="bg-white" onClick={exportPng}>
+						Pobierz grafikę
+					</button>
+				</div>
+			</Wrapper>
 		</section>
 	);
 };
