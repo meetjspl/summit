0a4099a58d9c515749d785936b5b98d8c9272d64
Kamil Dzieniszewski <kamil.d@playsmart.io>
2026-02-24 16:24:35 +0100

    feat: add TypeScript Boost Cards gallery with lightbox viewer

--- src/components/tickets.tsx
+++ src/components/tickets.tsx
@@ -1,4 +1,5 @@
 import { TicketVariant } from '@/components/ticket-variant.tsx';
+import { TypescriptBoostCards } from '@/components/typescript-boost-cards.tsx';
 import { Wrapper } from '@/components/wrapper.tsx';
 
 export const Tickets = () => {
@@ -26,12 +27,7 @@ export const Tickets = () => {
 							'Access to the coffee area',
 							'Seating in VIP rows (2-4)',
 							'Lunch at the bistro',
-							<a
-								href="https://instytutfullstack.pl/typescript-boost-cards"
-								className="text-meetjs-green underline"
-							>
-								TypeScript Boost Cards from Instytut Fullstack
-							</a>,
+							<TypescriptBoostCards />,
 							<p>
 								-10% for BRAVE courses:{' '}
 								<ul className="list-disc pl-6">
@@ -61,7 +57,6 @@ export const Tickets = () => {
 									</li>
 								</ul>
 							</p>,
-							'More benefits soon...',
 						]}
 						price={512}
 						highlight={true}
