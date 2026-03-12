9b1ddfe07a27c2ec72b1bda382b9ac4c1c6bd58d
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2026-03-12 09:04:46 +0100

    feat: add event summary page (#235)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import {Agenda} from '@/components/agenda.tsx';
-import {BecomeASponsor} from '@/components/become-a-sponsor.tsx';
-import {Charity} from '@/components/charity.tsx';
-import {Debate} from '@/components/debate.tsx';
-import {LoveLetter} from '@/components/love-letter.tsx';
-import {OrganizersSection} from '@/components/organizers-section.tsx';
-import {Partners} from '@/components/partners.tsx';
-import {PhotosSlider} from '@/components/photos-slider.tsx';
-import {Speakers} from '@/components/speakers.tsx';
-import {Venue} from '@/components/venue.tsx';
-import {VideoPlaylists} from '@/components/video-playlists.tsx';
-import {YoutubeShorts} from '@/components/youtube-shorts.tsx';
-import {useErrorTracking} from '@/hooks/useErrorTracking';
-import {useScrollDepthTracking} from '@/hooks/useScrollDepthTracking';
-import {useTimeOnPageTracking} from '@/hooks/useTimeOnPageTracking';
+import { Agenda } from '@/components/agenda.tsx';
+import { BecomeASponsor } from '@/components/become-a-sponsor.tsx';
+import { Charity } from '@/components/charity.tsx';
+import { Debate } from '@/components/debate.tsx';
+import { LoveLetter } from '@/components/love-letter.tsx';
+import { OrganizersSection } from '@/components/organizers-section.tsx';
+import { Partners } from '@/components/partners.tsx';
+import { PhotosSlider } from '@/components/photos-slider.tsx';
+import { Speakers } from '@/components/speakers.tsx';
+import { Venue } from '@/components/venue.tsx';
+import { VideoPlaylists } from '@/components/video-playlists.tsx';
+import { YoutubeShorts } from '@/components/youtube-shorts.tsx';
+import { useErrorTracking } from '@/hooks/useErrorTracking';
+import { useScrollDepthTracking } from '@/hooks/useScrollDepthTracking';
+import { useTimeOnPageTracking } from '@/hooks/useTimeOnPageTracking';
 
-import {Hero} from './components/hero.tsx';
+import { Hero } from './components/hero.tsx';
 
 export const App = () => {
 	useScrollDepthTracking();
